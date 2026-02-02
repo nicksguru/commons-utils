83ec7800521c1ef6d46c8655af84d020bebeb6ec
Nick S. <job-offers@nicks.guru>
2026-02-02 12:26:49 +0300

    refactor(utils)!: renamed method, added CompletableFuture failure logging

--- src/main/java/guru/nicks/commons/utils/FutureUtils.java
+++ src/main/java/guru/nicks/commons/utils/FutureUtils.java
@@ -1,6 +1,7 @@
 package guru.nicks.commons.utils;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.MDC;
 
@@ -11,6 +12,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -18,19 +20,20 @@ import java.util.function.Supplier;
  * Future-related utility methods.
  */
 @UtilityClass
+@Slf4j
 public class FutureUtils {
 
     /**
      * The number of virtual threads is unbounded, so sharing the executor everywhere is the intended usage pattern. To
-     * such methods as {@link CompletableFuture#thenAccept(Consumer)}, pass {@link #createMdcAwareExecutor()} in order
-     * to inherit (and therefore output in logs) parent thread's {@link MDC}.
+     * such methods as {@link CompletableFuture#thenAccept(Consumer)}, pass {@link #captureMdcForChildThreads()} in
+     * order to inherit (and therefore log) parent thread's {@link MDC}.
      *
-     * @see #createMdcAwareExecutor()
+     * @see #captureMdcForChildThreads()
      */
     public static final Executor VIRTUAL_THREAD_EXECUTOR = Executors.newVirtualThreadPerTaskExecutor();
 
     /**
-     * Same as {@link #getInParallel(Collection, int)}, just the number of threads equals the number of tasks.
+     * Same as {@link #getInParallel(Collection, int)}, just the batch size equals the number of tasks.
      *
      * @param tasks tasks to run
      * @return results of completed futures - in the same order as in the input collection
@@ -40,50 +43,61 @@ public class FutureUtils {
     }
 
     /**
-     * Creates {@link CompletableFuture} for each {@link Supplier}, runs them in parallel (with no more than
-     * {@code limit} virtual threads at a time), and awaits their completion. The point is to limit not the number of
-     * threads (virtual threads scale up to millions normally), but the number of memory- and/or IO-heavy workers.
+     * Creates {@link CompletableFuture} for each {@link Supplier}, runs them in parallel (no more than
+     * {@code maxConcurrentTasks} at a time), and awaits their completion. The point is to limit not the number of
+     * threads (virtual threads scale up to millions easily), but the number of memory- and/or IO-heavy workers.
      *
-     * @param tasks tasks to run
-     * @param limit max. number of threads
-     * @param <T>   future result type
+     * @param tasks              tasks to run
+     * @param maxConcurrentTasks max. number of concurrent (parallel) tasks
+     * @param <T>                future result type
      * @return results of completed futures - in the same order as in the input collection
      * @throws IllegalArgumentException the task limit is not positive (only if the task list is not empty)
      */
-    public static <T> List<T> getInParallel(Collection<Supplier<T>> tasks, int limit) {
+    public static <T> List<T> getInParallel(Collection<Supplier<T>> tasks, int maxConcurrentTasks) {
         // early return for speedup
         if (CollectionUtils.isEmpty(tasks)) {
             return List.of();
         }
 
-        if (limit <= 0) {
-            throw new IllegalArgumentException("Thread limit should be positive");
+        if (maxConcurrentTasks <= 0) {
+            throw new IllegalArgumentException("Max. number of concurrent tasks should be positive");
         }
 
         // in Java 17, there is 'newFixedThreadPool(limit)', but for virtual threads, no thread limiting is available,
         // hence a semaphore for concurrency limiting
-        var semaphore = new Semaphore(limit, true);
-        var executor = createMdcAwareExecutor();
+        var semaphore = new Semaphore(maxConcurrentTasks);
+        var executor = captureMdcForChildThreads();
+        var i = new AtomicInteger();
 
         List<CompletableFuture<T>> futures = tasks.stream()
                 .filter(Objects::nonNull)
                 .map(task -> {
-                    // acquire permit BEFORE creating the future - to actually limit concurrent task submissions
+                    // acquire permit BEFORE creating the future
                     try {
                         semaphore.acquire();
-                    } catch (InterruptedException e) {
+                    }
+                    // WARNING: this means already submitted tasks will complete, but their results will be lost
+                    catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                         throw new IllegalStateException("Thread interrupted while acquiring semaphore permit: "
                                 + e.getMessage(), e);
                     }
 
-                    return CompletableFuture.supplyAsync(() -> {
-                        try {
-                            return task.get();
-                        } finally {
-                            semaphore.release();
-                        }
-                    }, executor);
+                    int taskNumber = i.incrementAndGet();
+
+                    // In a very unlikely event when supplyAsync() fails during task submission, no future is created,
+                    // and whenComplete() isn't called, so the semaphore is never released. But this doesn't require
+                    // handling, as the outer loop fails and the exception propagates to caller.
+                    return CompletableFuture
+                            .supplyAsync(task, executor)
+                            .whenComplete((result, throwable) -> {
+                                if (throwable != null) {
+                                    log.error("Task {}/{} failed: {}", taskNumber, tasks.size(),
+                                            throwable.getMessage(), throwable);
+                                }
+
+                                semaphore.release();
+                            });
                 })
                 .toList();
 
@@ -96,7 +110,7 @@ public class FutureUtils {
     }
 
     /**
-     * Same as {@link #runInParallel(Collection, int)}, just the number of threads equals the number of tasks.
+     * Same as {@link #runInParallel(Collection, int)}, just the batch size equals the number of tasks.
      *
      * @param tasks tasks to run
      */
@@ -105,39 +119,36 @@ public class FutureUtils {
     }
 
     /**
-     * Creates {@link CompletableFuture} for each {@link Runnable}, runs them in parallel (but with no more than
-     * {@code limit} virtual threads at a time), and awaits their completion.
+     * Same as {@link #getInParallel(Collection, int)}, just no results are returned.
      *
-     * @param tasks tasks to run
-     * @param limit max. number of threads
+     * @param tasks              tasks to run
+     * @param maxConcurrentTasks max. number of concurrent (parallel) tasks
      */
-    public static void runInParallel(Collection<? extends Runnable> tasks, int limit) {
+    public static void runInParallel(Collection<? extends Runnable> tasks, int maxConcurrentTasks) {
         List<Supplier<Void>> suppliers = tasks.stream()
                 .map(TransformUtils::toSupplier)
                 .toList();
-        getInParallel(suppliers, limit);
+        getInParallel(suppliers, maxConcurrentTasks);
     }
 
     /**
-     * Wraps {@link #VIRTUAL_THREAD_EXECUTOR} executor to automatically propagate {@link MDC} context from caller thread
-     * to child threads. This ensures that all tasks will have access to the MDC context that was present when the task
-     * was submitted. The context variables (such as current user ID, request ID, etc.) can be output in logs using the
-     * {@code %X} placeholder.
+     * Wraps {@link #VIRTUAL_THREAD_EXECUTOR} executor to automatically propagate a snapshot of caller thread's
+     * {@link MDC} context to child threads and clear it upon task completion. The context variables can be logged using
+     * the {@code %X} placeholder.
      * <p>
-     * The MDC <b>context is captured only once</b> - when this method is called, from the caller's thread. Later on,
-     * it's passed to all tasks.
+     * The MDC <b>context is captured only once</b> - when this method is called.
      * <p>
      * Usage example:
      * <pre>
      *  // inherit MDC from this thread
-     *  var executor = FutureUtils.createMdcAwareExecutor();
+     *  var executor = FutureUtils.captureMdcForChildThreads();
      *  // pass MDC to new threads
      *  var future = CompletableFuture.supplyAsync(() -> someService.someMethod(), executor);
      * </pre>
      *
      * @return executor
      */
-    public static Executor createMdcAwareExecutor() {
+    public static Executor captureMdcForChildThreads() {
         var parentMdc = MDC.getCopyOfContextMap();
 
         return (Runnable task) -> VIRTUAL_THREAD_EXECUTOR.execute(() -> {

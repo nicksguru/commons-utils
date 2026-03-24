fe73f149d57423550757381cafb8d7bf7b56402a
Nick S. <job-offers@nicks.guru>
2026-03-24 19:50:11 +0300

    refactor: remove eager stringification, add benchmark, add fancy formatting

--- src/test/java/guru/nicks/commons/benchmark/ChecksumUtilsBenchmark.java
+++ src/test/java/guru/nicks/commons/benchmark/ChecksumUtilsBenchmark.java
@@ -4,11 +4,15 @@ import guru.nicks.commons.utils.crypto.ChecksumUtils;
 
 import lombok.Getter;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
 
 import java.util.LinkedHashMap;
@@ -20,10 +24,13 @@ import static guru.nicks.commons.validation.dsl.ValiDsl.checkNotNull;
 /**
  * Benchmark for {@link ChecksumUtils#computeJsonChecksum(Object)}.
  */
-@State(Scope.Benchmark)
-@Fork(2)
-@Measurement(iterations = 3, time = 3, timeUnit = TimeUnit.SECONDS)
-@Warmup(iterations = 2, time = 3, timeUnit = TimeUnit.SECONDS)
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@Threads(1)
 public class ChecksumUtilsBenchmark {
 
     private TestObject smallObject;

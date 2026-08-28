39a131a40956ac49689dfd89cd3f505c7474a87f
Nick S. <job-offers@nicks.guru>
2026-08-28 16:11:10 +0300

    perf(design-patterns): cache class' CSV schema for 24h

--- src/main/java/guru/nicks/commons/utils/text/CsvUtils.java
+++ src/main/java/guru/nicks/commons/utils/text/CsvUtils.java
@@ -1,5 +1,7 @@
 package guru.nicks.commons.utils.text;
 
+import guru.nicks.commons.cache.domain.CacheConstants;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.MappingIterator;
@@ -7,10 +9,13 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvParser;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.experimental.UtilityClass;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Duration;
 
 /**
  * CSV-related utility methods.
@@ -30,6 +35,17 @@ public class CsvUtils {
             .build();
 
     /**
+     * Fully-composed header schemas per class (schema derivation performs full bean introspection, but CsvSchema is
+     * immutable and deterministic per class).
+     *
+     * @see #parseCsv(InputStream, Class)
+     */
+    private static final Cache<Class<?>, CsvSchema> CSV_SCHEMA_CACHE = Caffeine.newBuilder()
+            .maximumSize(CacheConstants.DEFAULT_CAFFEINE_CACHE_CAPACITY)
+            .expireAfterAccess(Duration.ofHours(24))
+            .build();
+
+    /**
      * Converts input stream to objects. The first line must contain headers (case-insensitive property names).
      *
      * @param is    input stream
@@ -39,9 +55,7 @@ public class CsvUtils {
      * @throws IllegalArgumentException error during parsing
      */
     public static <T> MappingIterator<T> parseCsv(InputStream is, Class<T> clazz) {
-        CsvSchema schema = CSV_MAPPER.schemaFor(clazz)
-                .withHeader()
-                .withColumnReordering(true);
+        CsvSchema schema = CSV_SCHEMA_CACHE.get(clazz, CsvUtils::buildSchemaWithoutCache);
 
         try {
             return CSV_MAPPER.readerWithSchemaFor(clazz)
@@ -50,6 +64,19 @@ public class CsvUtils {
         } catch (IOException e) {
             throw new IllegalArgumentException(e.getMessage(), e);
         }
+    }
+
+    /**
+     * Cache loader for {@link #parseCsv(InputStream, Class)}: derives the fully-composed header schema for the given
+     * class.
+     *
+     * @param clazz item class
+     * @return immutable schema with header and column reordering enabled
+     */
+    private static CsvSchema buildSchemaWithoutCache(Class<?> clazz) {
+        return CSV_MAPPER.schemaFor(clazz)
+                .withHeader()
+                .withColumnReordering(true);
     }
 
 }
